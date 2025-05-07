3e3026381eac28239c5a15b6d00a9ccbc457a04d
exsaol24 <117499429+exsaol24@users.noreply.github.com>
2025-05-07 20:59:30 +0200

    feat and style ✨💎

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PlatosRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class InicioController extends AbstractController
 {
     #[Route('/inicio', name: 'app_inicio')]
-    public function index(): Response
+    public function index(PlatosRepository $platosRepository): Response
     {
+        // Obtener los platos desde la base de datos
+        $platos = $platosRepository->findAll();
+
+        // Renderizar la plantilla y pasar la variable 'platos'
         return $this->render('inicio/index.html.twig', [
-            'controller_name' => 'InicioController',
+            'platos' => $platos,
         ]);
     }
 }
